418ea239299264aa32165aa2ea66f2c10f797c5c
taiweituan <taiweituan@gmail.com>
2022-01-21 00:53:44 -0600

    added typing effect

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from 'react'
 import { Container } from 'react-bootstrap'
 
+const TEXT_TO_TYPE =
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo fugit ut eaque, maxime dicta hic totam ab error nostrum exercitationem consectetur recusandae repellendus voluptas illo amet laboriosam delectus, tempore vero.'
+
 export default function Intro() {
+  const [typeText, setTypeText] = useState('')
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      console.log('typing stuff')
+      setTypeText(TEXT_TO_TYPE.slice(0, typeText.length + 1))
+    }, 200)
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [typeText])
+
   return (
     <Container fluid id="section-1" className="px-4 py-5 text-center">
       <div className="col-xs-6">
@@ -8,6 +24,7 @@ export default function Intro() {
         <div className="profile-pic"></div>
         {/* <img src="~/src/assets/profile-pic.jpeg" alt="test" /> */}
         <p>A web developer and probably a video game addic</p>
+        <div className="blinking-cursor">{typeText}</div>
       </div>
     </Container>
   )
