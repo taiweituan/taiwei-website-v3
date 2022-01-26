a4c4d35e54270e147ae5889bc668d7432f02e561
taiweituan <taiweituan@gmail.com>
2022-01-26 00:17:05 -0600

    Added ability to add next line for typing effect

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,14 +1,15 @@
 import { Container } from 'react-bootstrap'
 import { useTypingTextEffect } from '../utils/useTypingTextEffect'
 
-const TEXT_TO_TYPE = ['Lorem ipsum dolor sit']
+const TEXT_TO_TYPE = ['Lorem ipsum', 'dolor']
+// const TEXT_TO_TYPE = ['Lorem ipsum dolor', 'second sentence']
 
 export default function Intro(): JSX.Element {
   const typingTextEffect = useTypingTextEffect(TEXT_TO_TYPE)
   return (
     <Container fluid id="section-1" className="px-4 py-5 text-center">
       <div className="col-xs-6">
-        <h1>Taiwei from Taiwan</h1>
+        <h1>Hello, This is Taiwei!</h1>
         <div className="profile-pic"></div>
         {/* <img src="~/src/assets/profile-pic.jpeg" alt="test" /> */}
         <p>A web developer and probably a video game addic</p>
--- src/utils/useTypingTextEffect.tsx
+++ src/utils/useTypingTextEffect.tsx
@@ -1,20 +1,38 @@
 import { useEffect, useState } from 'react'
 
 export const useTypingTextEffect = (sentences: string[]): string => {
-  const [typeText, setTypeText] = useState<string>('')
-  useEffect(() => {
-    const nextTextToType = sentences[0].slice(0, typeText.length + 1)
+  const [accumulatedSentence, setAccumulatedSentence] = useState<string>('')
+  const [currentIndex, setCurrentIndex] = useState<number>(0)
+  const [currentSentenceIndex, setCurrentSentenceIndex] = useState(0)
 
-    if (nextTextToType === typeText) return
+  useEffect(() => {
+    if (currentIndex === sentences.length) return
+
+    const currentSetence = sentences[currentIndex]
+    const nextCharToType = currentSetence[currentSentenceIndex]
 
     const timeout = setTimeout(() => {
-      setTypeText(nextTextToType)
-    }, 200)
+      const isEndOfSentenceReached =
+        currentSentenceIndex === currentSetence.length - 1
+      const isEndReached =
+        isEndOfSentenceReached && currentIndex + 1 === sentences.length
+
+      if (isEndOfSentenceReached) {
+        setAccumulatedSentence(
+          `${accumulatedSentence}${nextCharToType}${isEndReached ? '' : '\n'}`,
+        )
+        setCurrentIndex(currentIndex + 1)
+        setCurrentSentenceIndex(0)
+      } else {
+        setAccumulatedSentence(`${accumulatedSentence}${nextCharToType}`)
+        setCurrentSentenceIndex(currentSentenceIndex + 1)
+      }
+    }, 100)
 
     return () => {
       clearTimeout(timeout)
     }
-  }, [typeText])
+  }, [accumulatedSentence, currentSentenceIndex, currentIndex])
 
-  return typeText
+  return accumulatedSentence
 }
